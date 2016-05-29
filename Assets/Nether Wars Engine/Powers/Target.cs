d5f00cf09ad854aa3de104f1f60b5c4d8bee5791
Barnaff <kobi.chariski@gmail.com>
2016-05-30 02:22:29 +0300

    fixed target bitmasking to match unity enum mask popup field

--- Assets/Nether Wars Engine/Powers/Target.cs
+++ Assets/Nether Wars Engine/Powers/Target.cs
@@ -3,20 +3,21 @@
 namespace NetherWars.Powers
 {
 	[System.Flags]
-	public enum eTargetType
-	{
-		None 				= 0x000,
-        This			    = 0x001,
-        ThisController 		= 0x002,
-        FriendlyCreature    = 0x004,
-        EnemyCreature       = 0x008,
-        EnemyPlayer         = 0x010,
-        Friendly            = (FriendlyCreature | ThisController),
-        AnyPlayer			= (ThisController | EnemyPlayer),
-		AnyCreature			= (EnemyCreature |FriendlyCreature),
-		Enemy 				= (EnemyCreature | EnemyPlayer),
-        Any                 = (Friendly | Enemy)
+    public enum eTargetType
+    {
+        None,
+        This,
+        ThisController,
+        FriendlyCreature,
+        EnemyCreature,
+        EnemyPlayer,
+        Friendly = (FriendlyCreature | ThisController),
+        AnyPlayer = (ThisController | EnemyPlayer),
+        AnyCreature = (EnemyCreature | FriendlyCreature),
+        Enemy = (EnemyCreature | EnemyPlayer),
+        Any = (Friendly | Enemy)
     }
+
 
 	[System.Serializable]
     public class Target
